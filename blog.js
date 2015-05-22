2c54b2c06cce860688126139e287a15d9b1bd3e3
Tom Schmidt <z0phim@yahoo.com>
2015-05-22 12:15:41 -0700

    Added ability to create posts if logged in.

--- blog.js
+++ blog.js
@@ -1,23 +1,51 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Posts = new Mongo.Collection("posts");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+  Meteor.subscribe("posts");
+
+  Template.body.helpers({
+    posts: function () {
+      return Posts.find({}, {sort: {createdAt: -1 }});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-post": function (event) {
+      var title = event.target.title.value;
+      var content = event.target.content.value;
+
+      Meteor.call("addPost", title, content);
+
+      event.target.title.value = "";
+      event.target.content.value = "";
+
+      return false;
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "EMAIL_ONLY"
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.publish("posts", function () {
+      return Posts.find({});
+    });
   });
 }
+
+Meteor.methods({
+  addPost: function (title, content) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+    Posts.insert({
+      title: title,
+      content: content,
+      createdAt: new Date(),
+      owner: Meteor.userId()
+    });
+  }
+});
